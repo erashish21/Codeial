d570a2c267e4d48d8795214eb4b383a03757c35c
Ashish bundela <ashishbundela123@gmail.com>
2023-05-24 16:17:16 +0530

    redis install

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://ashishbundela123:vCM4r6Noe4eeKnfV@cluster0.htozyd1.mongodb.net/codeial');
-const db = mongoose.connection;
-db.on('error',console.error.bind(console,'error coneecting to db'));
-
-db.once('open',function(){
-    
-    console.log('Successfully connected to the database');
+mongoose.set("strictQuery", false);
+mongoose.connect(
+    'mongodb+srv://ashishbundela123:vCM4r6Noe4eeKnfV@cluster0.htozyd1.mongodb.net/codeial',
+   () => {
+  console.log("Connected to MongoDB");
 });
